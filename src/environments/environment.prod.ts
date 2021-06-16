0701fa84e1f6fbfe488c7e84cfd3fe4fb41e9941
Joel Tang <dangjoeltang@gmail.com>
2021-06-16 13:51:33 -0500

    fix scss imports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBqIq7Tn0SkpQTuoyzK91OCDuvqCEZAyo0",
+    authDomain: "terracotta-oms.firebaseapp.com",
+    projectId: "terracotta-oms",
+    storageBucket: "terracotta-oms.appspot.com",
+    messagingSenderId: "459619082569",
+    appId: "1:459619082569:web:7ca0f2a855f8fb88c0fa98",
+    measurementId: "G-CWNP1FLTY1"
+  }
 };
